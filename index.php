30a3eac68bc58c375c34e04662d419bd9b97d02a
Vinicius José <vinicius.jsilv@gmail.com>
2021-04-30 23:13:36 -0300

    Criaçao da conexao com o banco de dados, e criaçao da tabela de visualizaçao

---
+++ index.php
@@ -0,0 +1,10 @@
+<table border = "1" width = "400">
+    <tr>
+        <th>Nome</th>
+        <th>Idade</th>
+    </tr>
+    <?php
+        $sql = "SELECT * FROM usuarios";
+         
+    ?>
+</table>
