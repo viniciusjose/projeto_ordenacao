e8ca979ce842afa4e34de81a6ebe62b4fb1e863c
Vinicius José <vinicius.jsilv@gmail.com>
2021-04-30 23:34:13 -0300

    Mudança no metodo de conexao ao banco de dados, e adicionado a populaçao da tabela diretamente do banco de dados

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
-<table border = "1" width = "400">
-    <tr>
-        <th>Nome</th>
-        <th>Idade</th>
-    </tr>
-    <?php
-        $sql = "SELECT * FROM usuarios";
-         
-    ?>
+<?php
+try {
+	$pdo = new PDO("mysql:dbname=projeto_ordenar;host=localhost", "root", "");
+} catch(PDOException $e) {
+	echo "ERRO: ".$e->getMessage();
+	exit;
+}
+?>
+
+
+<table border="1" width="400">
+	<tr>
+		<th>Nome</th>
+		<th>Idade</th>
+	</tr>
+	<?php
+	
+    $sql = "SELECT * FROM usuarios";
+	$sql = $pdo->query($sql);
+	if($sql->rowCount() > 0) {
+
+		foreach($sql->fetchAll() as $usuario):
+			?>
+
+			<tr>
+				<td><?php echo $usuario['nome']; ?></td>
+				<td><?php echo $usuario['idade']; ?></td>
+			</tr>
+
+			<?php
+		endforeach;
+
+	}
+	?>
 </table>
